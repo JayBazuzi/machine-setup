90280b7d077090f0645a2f43772cc03b1ca7c7ac
Jay Bazuzi <jay@bazuzi.com>
2020-09-19 12:14:59 -0700

    Create windows.ps1

---
+++ windows.ps1
@@ -0,0 +1,37 @@
+#Requires -RunAsAdministrator
+
+iwr -useb cin.st | iex
+choco feature enable --name=allowGlobalConfirmation
+cinst win-no-annoy
+cinst vscode
+cinst googlechrome
+cinst git poshgit github-desktop
+cinst notepadplusplus
+cinst beyondcompare
+cinst nuget.commandline
+
+# delete annoying Windows notification sounds
+Remove-Item -Recurse HKCU:\AppEvents\Schemes
+Set-Service Audiosrv -StartupType Automatic
+
+# Show seconds in the clock so screen sharing latency is obvious to all
+Set-ItemProperty HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced ShowSecondsInSystemClock 1
+
+@(
+    'coenraads.bracket-pair-colorizer-2'
+    'wmaurer.change-case'
+    'streetsidesoftware.code-spell-checker'
+    'ryu1kn.partial-diff'
+    'ms-vscode.powershell'
+    'mohsen1.prettify-json'
+    'vscode-icons-team.vscode-icons'
+) | % { & "C:\Program Files\Microsoft VS Code\bin\code.cmd" --install-extension $_ }
+
+$ProgressPreference = 'SilentlyContinue'
+$mobtimeVersion = '1.7.4'
+iwr https://github.com/GreatWebGuy/MobTime/releases/download/v$mobtimeVersion/MobTime-$mobtimeVersion.msi -O MobTime.msi
+./MobTime.msi /qr
+
+# Often fails because anydesk chocolate authoring is bad
+cinst anydesk
+
