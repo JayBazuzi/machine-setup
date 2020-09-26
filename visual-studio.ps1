9656c34435db7cf72c0680ade1761ea258ca55b7
Jay Bazuzi <jay@bazuzi.com>
2020-09-25 20:19:14 -0700

    Update visual-studio.ps1

--- visual-studio.ps1
+++ visual-studio.ps1
@@ -1,8 +1,10 @@
 #Requires -RunAsAdministrator
 
 iwr -useb cin.st | iex
+choco feature enable --name=allowGlobalConfirmation
 cinst visualstudio2019professional visualstudio2019-workload-manageddesktop netfx-4.8-devpack
 cinst resharper-ultimate-all ncrunch-vs2019
+cinst nuget.commandline
 
 # Need a way to automate installing this package
 & "${env:ProgramFiles}\Google\Chrome\Application\chrome.exe" https://marketplace.visualstudio.com/items?itemName=HangjitRai.AutoSaveFile
