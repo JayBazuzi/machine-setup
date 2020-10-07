673ccaca3c9526ef659fb0ce5aa50ac732eadb99
Llewellyn Falco <llewellyn.falco@gmail.com>
2020-10-06 21:04:43 -0600

    Create full.net.machine.ps1

---
+++ full.net.machine.ps1
@@ -0,0 +1,10 @@
+
+#This file
+# iwr -useb https://raw.githubusercontent.com/JayBazuzi/machine-setup/main/windows.ps1 | iex
+
+
+# install mobbing-base
+iwr -useb https://raw.githubusercontent.com/JayBazuzi/machine-setup/main/windows.ps1 | iex
+
+# install visual-studio
+iwr -useb https://raw.githubusercontent.com/JayBazuzi/machine-setup/main/visual-studio.ps1 | iex
