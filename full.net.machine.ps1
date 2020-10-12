d4e3690c4a4b0e6fc512f9030b2b2ba592b35853
Jay Bazuzi <jay@bazuzi.com>
2020-10-11 21:55:52 -0700

    Update full.net.machine.ps1

--- full.net.machine.ps1
+++ full.net.machine.ps1
@@ -1,10 +1,5 @@
+# How to run this file:
+#  PS> iwr -useb https://raw.githubusercontent.com/JayBazuzi/machine-setup/main/full.net.machine.ps1 | iex
 
-#This file
-# iwr -useb https://raw.githubusercontent.com/isidore/machine-setup/main/full.net.machine.ps1 | iex
-
-
-# install mobbing-base
 iwr -useb https://raw.githubusercontent.com/JayBazuzi/machine-setup/main/windows.ps1 | iex
-
-# install visual-studio
 iwr -useb https://raw.githubusercontent.com/JayBazuzi/machine-setup/main/visual-studio.ps1 | iex
