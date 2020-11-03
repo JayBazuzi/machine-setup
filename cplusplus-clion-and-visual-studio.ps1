f7356d768d2bfc943a08c02e571f97edbbc13ebd
Jacqueline Lee <jmasonlee@gmail.com>
2020-11-02 17:09:03 -0700

    Ad C++

---
+++ cplusplus-clion-and-visual-studio.ps1
@@ -0,0 +1,37 @@
+# To run this script directly, run this in an elevated admin PowerShell prompt:
+# iwr -useb https://raw.githubusercontent.com/JayBazuzi/machine-setup/main/cplusplus-clion-and-visual-studio.ps1 | iex
+
+#Requires -RunAsAdministrator
+
+iwr -useb cin.st | iex
+choco feature enable --name=allowGlobalConfirmation
+
+# -------------------------------------------------------
+# Diff Tools
+cinst  tortoisesvn
+
+# -------------------------------------------------------
+# IDEs and editors
+cinst clion-ide
+cinst visualstudio2019community
+cinst visualstudio2019-workload-nativedesktop
+
+cinst resharpercpp
+
+# -------------------------------------------------------
+# Build tools
+cinst cmake.install --installargs 'ADD_CMAKE_TO_PATH=System'
+cinst ninja conan
+
+# -------------------------------------------------------
+# CygWin
+# cinst cygwin
+# C:\tools\cygwin\cygwinsetup.exe  --quiet-mode --packages gcc-core
+# C:\tools\cygwin\cygwinsetup.exe  --quiet-mode --packages make
+# C:\tools\cygwin\cygwinsetup.exe  --quiet-mode --packages gcc-g++
+
+# -------------------------------------------------------
+# mingw
+# cinst mingw
+
+Write-Host -Foreground yellow "Reboot when done"
---
+++ dev_environments/cplusplus.ps1
@@ -0,0 +1,9 @@
+# How to run this file:
+#  PS> iwr -useb https://raw.githubusercontent.com/JayBazuzi/machine-setup/main/dev-environments/cplusplus.ps1 | iex
+
+iwr -useb https://raw.githubusercontent.com/JayBazuzi/machine-setup/main/windows.ps1 | iex
+iwr -useb https://raw.githubusercontent.com/JayBazuzi/machine-setup/main/cplusplus-clion-and-visual-studio.ps1 | iex
+
+# Clone repo
+& "C:\Program Files\Git\cmd\git.exe" clone https://github.com/approvals/ApprovalTests.cpp.StarterProject.git C:\Code\ApprovalTests.cpp.StarterProject
+
