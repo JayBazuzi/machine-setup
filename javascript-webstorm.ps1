a8b632678f5e8087c0c888f34d776a1738fef614
Llewellyn Falco <llewellyn.falco@gmail.com>
2020-11-09 10:11:52 -0700

    Update javascript-webstorm.ps1

--- javascript-webstorm.ps1
+++ javascript-webstorm.ps1
@@ -1,10 +1,12 @@
 # This script
-# iwr -useb https://raw.githubusercontent.com/JayBazuzi/machine-setup/main/java-intellij.ps1 | iex
+# iwr -useb https://raw.githubusercontent.com/JayBazuzi/machine-setup/main/javascript-webstorm.ps1 | iex
 
 #Requires -RunAsAdministrator
 
 iwr -useb cin.st | iex
 choco feature enable --name=allowGlobalConfirmation
-choco install beyondcompare openjdk intellijidea maven
+cinst nodejs
+cinst yarn
+cinst webstorm
 
 Write-Host -Foreground yellow "Reboot when done"
