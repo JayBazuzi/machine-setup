4c95fd350b65b0d7da4555cea9886476a614ea61
Simon <29654835+absolutelysimon@users.noreply.github.com>
2020-12-21 18:06:46 -0700

    Fixing a typo in the Python setup file.

--- dev_environments/python.ps1
+++ dev_environments/python.ps1
@@ -1,5 +1,5 @@
 # How to run this file:
-#  PS> iwr -useb https://raw.githubusercontent.com/JayBazuzi/machine-setup/main/dev-environments/python.ps1 | iex
+#  PS> iwr -useb https://raw.githubusercontent.com/JayBazuzi/machine-setup/main/dev_environments/python.ps1 | iex
 
 iwr -useb https://raw.githubusercontent.com/JayBazuzi/machine-setup/main/windows.ps1 | iex
 iwr -useb https://raw.githubusercontent.com/JayBazuzi/machine-setup/main/python-pycharm.ps1 | iex
