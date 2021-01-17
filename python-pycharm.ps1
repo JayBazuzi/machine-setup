8b2fd54d51964e977e67f0cda40d1b301e861b0b
Llewellyn Falco <llewellyn.falco@gmail.com>
2021-01-17 09:38:12 -0700

    Update python-pycharm.ps1

--- python-pycharm.ps1
+++ python-pycharm.ps1
@@ -10,5 +10,6 @@ choco install python --pre
 choco install pip
 choco install pycharm
 
+syspin "C:\Program Files (x86)\JetBrains\PyCharm 2020.3.2\bin\pycharm64.exe" "Pin to taskbar"
 
 Write-Host -Foreground yellow "Reboot when done"
