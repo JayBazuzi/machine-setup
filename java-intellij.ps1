9ff3d4f29e244dc9ee11f8f7fa8e17419db78645
Llewellyn Falco <llewellyn.falco@gmail.com>
2021-01-18 13:53:54 -0700

    Update java-intellij.ps1

--- java-intellij.ps1
+++ java-intellij.ps1
@@ -7,4 +7,7 @@ iwr -useb cin.st | iex
 choco feature enable --name=allowGlobalConfirmation
 choco install beyondcompare openjdk intellijidea-community maven gradle
 
+cinst syspin --ignore-checksums
+syspin "C:\Program Files\JetBrains\IntelliJ IDEA Community Edition 2020.3.1\bin\idea64.exe" "Pin to taskbar"
+
 Write-Host -Foreground yellow "Reboot when done"
