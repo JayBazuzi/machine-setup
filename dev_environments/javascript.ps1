a52bcf628dde5ac38bbe5437d8da70d43bb326d7
Llewellyn Falco <llewellyn.falco@gmail.com>
2021-01-26 17:46:50 -0700

    Update javascript.ps1

--- dev_environments/javascript.ps1
+++ dev_environments/javascript.ps1
@@ -1,8 +1,6 @@
-# How to run this file:
-#  PS> iwr -useb https://raw.githubusercontent.com/JayBazuzi/machine-setup/main/dev_environments/javascript.ps1 | iex
+# ApprovalTests StarterProject
+# Invoke-WebRequest -UseBasicParsing https://raw.githubusercontent.com/approvals/ApprovalTests.js.StarterProject/master/install.windows.ps1 | Invoke-Expression
 
-iwr -useb https://raw.githubusercontent.com/JayBazuzi/machine-setup/main/windows.ps1 | iex
-iwr -useb https://raw.githubusercontent.com/JayBazuzi/machine-setup/main/javascript-webstorm.ps1 | iex
 
-# Clone repo
-& "C:\Program Files\Git\cmd\git.exe" clone https://github.com/approvals/ApprovalTests.js.StarterProject.git C:\Code\ApprovalTests.js.StarterProject
+#Jest-Es6 StarterProject 
+# iwr -useb https://raw.githubusercontent.com/jmasonlee/JavaScript.jest.es6.StarterProject/MachineSetup.windows.ps1 | iex
