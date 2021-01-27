adc995f9028309829137277f6fff0259ae98f4b1
Llewellyn Falco <llewellyn.falco@gmail.com>
2021-01-26 17:09:56 -0700

    Update javascript-webstorm.ps1

--- javascript-webstorm.ps1
+++ javascript-webstorm.ps1
@@ -10,4 +10,8 @@ choco install nodejs
 choco install yarn
 choco install webstorm
 
+cinst syspin --ignore-checksums
+syspin "C:\Program Files\JetBrains\IntelliJ IDEA Community Edition 2020.3.1\bin\idea64.exe" "Pin to taskbar"
+syspin "C:\Program Files (x86)\JetBrains\WebStorm 2020.3.2\bin\webstorm64.exe" "Pin to taskbar"
+
 Write-Host -Foreground yellow "Reboot when done"
