960fb14b098901902835d11b730868b49d64819a
Jay Bazuzi <jay@bazuzi.com>
2021-02-12 10:13:05 -0800

    Update cplusplus.ps1

--- dev_environments/cplusplus.ps1
+++ dev_environments/cplusplus.ps1
@@ -1,5 +1,5 @@
 # How to run this file:
-#  PS> iwr -useb https://raw.githubusercontent.com/JayBazuzi/machine-setup/main/dev-environments/cplusplus.ps1 | iex
+#  PS> iwr -useb https://raw.githubusercontent.com/JayBazuzi/machine-setup/main/dev_environments/cplusplus.ps1 | iex
 
 iwr -useb https://raw.githubusercontent.com/JayBazuzi/machine-setup/main/windows.ps1 | iex
 iwr -useb https://raw.githubusercontent.com/JayBazuzi/machine-setup/main/cplusplus-clion-and-visual-studio.ps1 | iex
