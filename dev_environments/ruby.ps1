737ea8def8f1d37bf0d49239e524a2c4d74acba5
jmasonlee <jmasonlee@gmail.com>
2021-03-09 12:46:20 -0700

    Use correct url for ruby

--- dev_environments/ruby.ps1
+++ dev_environments/ruby.ps1
@@ -1,5 +1,5 @@
 # How to run this file:
-#  PS> iwr -useb https://raw.githubusercontent.com/JayBazuzi/machine-setup/main/dev-environments/.ps1 | iex
+#  PS> iwr -useb https://raw.githubusercontent.com/JayBazuzi/machine-setup/main/dev_environments/ruby.ps1 | iex
 
 iwr -useb https://raw.githubusercontent.com/JayBazuzi/machine-setup/main/windows.ps1 | iex
 iwr -useb https://raw.githubusercontent.com/JayBazuzi/machine-setup/main/ruby-rubymine.ps1 | iex
