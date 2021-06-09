79de50ecb9eb83316558512d1f51a32946476663
jmasonlee <jmasonlee@gmail.com>
2021-06-09 07:51:26 -0600

    Add more descriptive step for installing plugin

--- dev_environments/haskell.ps1
+++ dev_environments/haskell.ps1
@@ -6,7 +6,7 @@ iwr -useb https://raw.githubusercontent.com/JayBazuzi/machine-setup/main/visual-
 
 choco install haskell-stack 
 cinst intellijidea
-# intellij haskell-force
+# In IntelliJ, install the hask-force plugin and restart
 
 #Running test does some install as well:
 # stack test
