df5964fade3260bf9a2cb1006d9447ecd45ca583
Jay Bazuzi <jay@bazuzi.com>
2021-08-10 21:23:20 -0700

    Add NuGet package source

--- visual-studio.ps1
+++ visual-studio.ps1
@@ -7,7 +7,9 @@ iwr -useb cin.st | iex
 choco feature enable --name=allowGlobalConfirmation
 cinst visualstudio2019professional visualstudio2019-workload-manageddesktop netfx-4.8-devpack
 cinst resharper-ultimate-all ncrunch-vs2019
+
 cinst nuget.commandline
+nuget.exe sources add -Name nuget.org -Source https://api.nuget.org/v3/index.json
 
 cinst syspin --ignore-checksums # has a broken checksum
 syspin "C:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\Common7\IDE\devenv.exe" "Pin to taskbar"
