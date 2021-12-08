662d741d1532854a8b47e7b382f7c753c51e45b8
Llewellyn Falco <llewellyn.falco@gmail.com>
2021-12-08 18:02:03 +0000

    Delete elixir.ps1

--- dev_environments/elixir.ps1
+++
@@ -1,5 +0,0 @@
-# How to run this file:
-#  PS> iwr -useb https://raw.githubusercontent.com/JayBazuzi/machine-setup/main/elixir.ps1 | iex
-
-iwr -useb https://raw.githubusercontent.com/JayBazuzi/machine-setup/main/windows.ps1 | iex
-iwr -useb https://raw.githubusercontent.com/JayBazuzi/machine-setup/main/visual-studio.ps1 | iex
