11e58ea5c97d3ec282e16a23c8a14e5cba73bc80
Jay Bazuzi <jay@bazuzi.com>
2022-03-15 06:00:10 -0700

    Create teamcity.ps1

---
+++ teamcity.ps1
@@ -0,0 +1,15 @@
+# This script
+# iwr -useb https://raw.githubusercontent.com/JayBazuzi/machine-setup/main/java-intellij.ps1 | iex
+
+#Requires -RunAsAdministrator
+
+iwr -useb cin.st | iex
+choco feature enable --name=allowGlobalConfirmation
+
+choco install beyondcompare openjdk intellijidea-community maven gradle
+choco install teamcity
+
+cinst syspin --ignore-checksums
+syspin "C:\Program Files\JetBrains\IntelliJ IDEA Community Edition 2020.3.1\bin\idea64.exe" "Pin to taskbar"
+
+Write-Host -Foreground yellow "Reboot when done"
